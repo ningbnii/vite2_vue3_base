1d68b359c7e423342b2b11c1fafb5078e29f13e0
ning.bnii@163.com <>
2022-01-05 16:58:02 +0800

    update

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - 自动化路由
 - 封装 axios，支持 JWT
 - 封装 localStorage
+- 移动端适配 postcss 插件 postcss-px2vp
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,37 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import styleImport from "vite-plugin-style-import";
+import px2vp from "postcss-px2vp";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    styleImport({
-      libs: [
-        {
-          libraryName: "vant",
-          esModule: true,
-          resolveStyle: (name) => `vant/es/${name}/style/index`,
+const vantStyleImport = () => {
+  const libraryName = "vant";
+  return styleImport({
+    libs: [
+      {
+        libraryName,
+        esModule: true,
+        resolveStyle(name) {
+          return `${libraryName}/es/${name}/style`;
         },
+      },
+    ],
+  });
+};
+
+export default defineConfig({
+  // 大佬方案https://juejin.cn/post/6961737808339795975
+  css: {
+    postcss: {
+      plugins: [
+        px2vp({
+          viewportWidth(rule) {
+            const file = rule.source?.input.file;
+            if (file?.includes("vant")) return 375;
+            return 750;
+          },
+        }),
       ],
-    }),
-  ],
+    },
+  },
+  plugins: [vue(), vantStyleImport()],
 });
