47e343c62b4c6857beec5c869133a6dc8a94d9f5
ning.bnii@163.com <>
2022-01-05 17:20:32 +0800

    添加less支持

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,11 +3,23 @@
   <div>
     <van-button type="primary">主要按钮</van-button>
   </div>
+  <div class="content">
+    <p>hello</p>
+  </div>
 </template>
 <script setup>
 import { getUserInfo } from "../api/user";
+// 请求接口demo
 getUserInfo({ id: 1 }).then(function (res) {
   console.log(res);
 });
 </script>
-<style lang=""></style>
+<style lang="less" scoped>
+.content {
+  width: 750px;
+  background-color: green;
+  p {
+    color: red;
+  }
+}
+</style>
