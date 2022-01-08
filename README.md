39038272d06afdbb0d833fb3390f87d7abd47f0b
ning <296720094@qq.com>
2022-01-08 13:10:27 +0800

    添加开发环境和生成环境中 base 的区分，生产环境.env，开发环境.env.production

--- README.md
+++ README.md
@@ -20,3 +20,4 @@ yarn build
 - 组件库使用 vant
 - 配置 server，添加 ip 访问
 - title 动态修改
+- 添加开发环境和生成环境中 base 的区分，生产环境.env，开发环境.env.production
--- src/main.js
+++ src/main.js
@@ -4,6 +4,4 @@ import router from "./router";
 import { Button } from "vant";
 import { createMetaManager } from "vue-meta";
 
-const app = createApp(App).use(router).use(Button).use(createMetaManager());
-await router.isReady();
-app.mount("#app");
+createApp(App).use(router).use(Button).use(createMetaManager()).mount("#app");
