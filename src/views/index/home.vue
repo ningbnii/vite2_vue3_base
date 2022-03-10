6ce7437631009c5c4d1130a218939ef8b2b5da3d
ning <296720094@qq.com>
2022-03-10 22:36:00 +0800

    update

--- src/views/index/home.vue
+++ src/views/index/home.vue
@@ -1,50 +1,33 @@
 <template>
-  <div class="container">
-    <header>
-      <h1 class="page-heading">Ink</h1>
-    </header>
-    <nav>
-      <ul class="site-nav">
-        <li><a href="">Home</a></li>
-        <li><a href="">Features</a></li>
-        <li><a href="">Pricing</a></li>
-        <li><a href="">Support</a></li>
-        <li class="nav-right"><a href="">About</a></li>
-      </ul>
-    </nav>
-
-    <main class="main tile">
-      <h1>Team collaboration done right</h1>
-      <p>
-        Thousands of teams from all over the world turn to <b>Ink</b>to
-        communicate and get things done.
-      </p>
-    </main>
-
-    <div class="sidebar-top tile">
-      <form action="" class="login-form">
-        <h3>Login</h3>
-        <p>
-          <label for="username">Username</label
-          ><input type="text" id="username" />
-        </p>
-        <p>
-          <label for="password">Password</label
-          ><input type="password" id="password" />
-        </p>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-
-    <div class="sidebar-bottom tile centered">
-      <small>Starting at</small>
-      <div class="cost">
-        <span class="cost-currency"></span>
-        <span class="cost-dollars"></span>
-        <span class="cost-cents"></span>
-      </div>
-      <a href="" class="cta-button">Sign up</a>
-    </div>
+  <div class="portfolio">
+    <figure class="featured">
+      <img src="../../assets/logo.png" alt="monkey" />
+      <figcaption>Monkey</figcaption>
+    </figure>
+    <figure>
+      <img src="../../assets/logo.png" alt="eagle" />
+      <figcaption>Eagle</figcaption>
+    </figure>
+    <figure class="featured">
+      <img src="../../assets/logo.png" alt="bird" />
+      <figcaption>Bird</figcaption>
+    </figure>
+    <figure>
+      <img src="../../assets/logo.png" alt="bear" />
+      <figcaption>Bear</figcaption>
+    </figure>
+    <figure class="featured">
+      <img src="../../assets/logo.png" alt="swan" />
+      <figcaption>Swan</figcaption>
+    </figure>
+    <figure>
+      <img src="../../assets/logo.png" alt="elephants" />
+      <figcaption>Elephants</figcaption>
+    </figure>
+    <figure>
+      <img src="../../assets/logo.png" alt="owl" />
+      <figcaption>Owl</figcaption>
+    </figure>
   </div>
 </template>
 <script setup>
@@ -54,39 +37,30 @@ useMeta({
 })
 </script>
 <style lang="scss">
-.container {
+.portfolio {
   display: grid;
-  grid-template-columns: 2fr 1fr;
-  grid-template-rows: repeat(4, auto); // 定义4条水平轨道，大小为auto
-  grid-gap: 1.5em;
-  max-width: 1080px;
-}
-
-header,
-nav {
-  grid-column: 1/3;
-  grid-row: span 1;
-}
-main {
-  grid-column: 1/2;
-  grid-row: 3/5;
-}
-.sidebar-top {
-  grid-column: 2/3;
-  grid-row: 3/4;
-}
-.sidebar-bottom {
-  grid-column: 2/3;
-  grid-row: 4/5;
-}
-.tile {
-  padding: 1.5em;
-  background-color: #fff;
-}
-.tile > :first-child {
-  margin-top: 0;
-}
-.tile * + * {
-  margin-top: 1.5em;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-auto-rows: 1fr;
+  grid-gap: 1em;
+  grid-auto-flow: dense;
+  figure {
+    display: flex;
+    flex-direction: column;
+    max-width: initial;
+    img {
+      flex: 1;
+      object-fit: cover;
+    }
+    figcaption {
+      padding: 0.3em 0.8em;
+      background-color: rgba(0, 0, 0, 0.5);
+      color: white;
+      text-align: right;
+    }
+  }
+  .featured {
+    grid-row: span 2;
+    grid-column: span 2;
+  }
 }
 </style>
